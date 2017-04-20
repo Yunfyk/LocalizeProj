74ccc51f4004bc3c5271c462d738644495ace394
zhusanbao <symbal_app@163.com>
2017-04-20 09:38:21 +0800

    cm 1

--- LocalizeProj/ViewController.m
+++ LocalizeProj/ViewController.m
@@ -17,6 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //commit 1
     [_clickSender setTitle:NSLocalizedString(@"btn", nil) forState:(UIControlStateNormal)];
     // Do any additional setup after loading the view, typically from a nib.
 }
