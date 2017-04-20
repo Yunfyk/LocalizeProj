345497e97e4375c48059697114e270e750be2461
zhusanbao <symbal_app@163.com>
2017-04-20 09:22:54 +0800

    localized string

---
+++ LocalizeProj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalizeProj
+//
+//  Created by 朱三保 on 2017/4/12.
+//  Copyright © 2017年 朱三保. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocalizeProj/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  LocalizeProj
+//
+//  Created by 朱三保 on 2017/4/12.
+//  Copyright © 2017年 朱三保. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *clickSender;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [_clickSender setTitle:NSLocalizedString(@"btn", nil) forState:(UIControlStateNormal)];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
